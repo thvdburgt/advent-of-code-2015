701ebddef18b58b21de8b19fc157584b4e738aaa
Thomas van der Burgt <thomas@thvdburgt.nl>
2017-12-15 23:05:06 +0100

    rustfmt

--- day-1-not-quite-lisp/src/lib.rs
+++ day-1-not-quite-lisp/src/lib.rs
@@ -1,25 +1,26 @@
 pub fn solve_puzzle_part_1(input: &str) -> i32 {
-    let floor = input.chars().fold(0, |floor, c| {
-        match c {
-            '(' => floor + 1,
-            ')' => floor - 1,
-            _   => panic!(),
-        }
+    let floor = input.chars().fold(0, |floor, c| match c {
+        '(' => floor + 1,
+        ')' => floor - 1,
+        _ => panic!(),
     });
 
     floor
 }
 
 pub fn solve_puzzle_part_2(input: &str) -> usize {
-    input.chars().scan(0, |floor, c| {
-        *floor = match c {
-            '(' => *floor + 1,
-            ')' => *floor - 1,
-            _   => panic!(),
-        };
-        Some(*floor)
-    })
-    .enumerate()
-    .find(|&(_, floor)| floor < 0).unwrap().0 + 1
+    input
+        .chars()
+        .scan(0, |floor, c| {
+            *floor = match c {
+                '(' => *floor + 1,
+                ')' => *floor - 1,
+                _ => panic!(),
+            };
+            Some(*floor)
+        })
+        .enumerate()
+        .find(|&(_, floor)| floor < 0)
+        .unwrap()
+        .0 + 1
 }
-
