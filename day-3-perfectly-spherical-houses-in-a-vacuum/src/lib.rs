14aaae54f0899ad2ed2771172378d237503f9a4d
Thomas van der Burgt <thomas@thvdburgt.nl>
2018-09-12 18:59:01 +0200

    solved day 3

---
+++ day-3-perfectly-spherical-houses-in-a-vacuum/src/lib.rs
@@ -0,0 +1,41 @@
+use std::collections::HashSet;
+
+fn visit(c: char, x: &mut i32, y: &mut i32, ref mut visited_locations: &mut HashSet<(i32, i32)>) {
+    match c {
+        '>' => *x += 1,
+        '<' => *x -= 1,
+        '^' => *y += 1,
+        'v' => *y -= 1,
+        _ => panic!("Unknown move: {}", c),
+    };
+    visited_locations.insert((*x, *y));
+}
+
+pub fn solve_puzzle_part_1(input: &str) -> usize {
+    let mut visited_locations = HashSet::new();
+    visited_locations.insert((0, 0));
+    let (mut x, mut y) = (0, 0);
+
+    for c in input.chars() {
+        visit(c, &mut x, &mut y, &mut visited_locations);
+    }
+
+    visited_locations.len()
+}
+
+pub fn solve_puzzle_part_2(input: &str) -> usize {
+    let mut visited_locations: HashSet<(i32, i32)> = HashSet::new();
+    visited_locations.insert((0, 0));
+    let (mut santa_x, mut santa_y) = (0, 0);
+    let (mut robo_x, mut robo_y) = (0, 0);
+
+    for (i, c) in input.char_indices() {
+        if i % 2 == 0 {
+            visit(c, &mut santa_x, &mut santa_y, &mut visited_locations);
+        } else {
+            visit(c, &mut robo_x, &mut robo_y, &mut visited_locations);
+        }
+    }
+
+    visited_locations.len()
+}
---
+++ day-3-perfectly-spherical-houses-in-a-vacuum/tests/tests.rs
@@ -0,0 +1,37 @@
+extern crate advent_of_code_2015_day_x;
+
+use advent_of_code_2015_day_x::*;
+
+#[test]
+fn part_1_example_1() {
+    let input = ">";
+    assert_eq!(solve_puzzle_part_1(input), 2);
+}
+
+#[test]
+fn part_1_example_2() {
+    let input = "^>v<";
+    assert_eq!(solve_puzzle_part_1(input), 4);
+}
+
+#[test]
+fn part_1_example_3() {
+    let input = "^v^v^v^v^v";
+    assert_eq!(solve_puzzle_part_1(input), 2);
+}
+
+#[test]
+fn part_2_example_1() {
+    let input = "^>";
+    assert_eq!(solve_puzzle_part_2(input), 3);
+}
+#[test]
+fn part_2_example_2() {
+    let input = "^>v<";
+    assert_eq!(solve_puzzle_part_2(input), 3);
+}
+#[test]
+fn part_2_example_3() {
+    let input = "^v^v^v^v^v";
+    assert_eq!(solve_puzzle_part_2(input), 11);
+}
