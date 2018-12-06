f19efd418a0f3f1d08fb278123299aff6c72c10a
Thomas van der Burgt <thomas@thvdburgt.nl>
2018-12-06 17:31:04 +0100

    refactor template

--- template/tests/tests.rs
+++ template/tests/tests.rs
@@ -1,6 +1,6 @@
-extern crate advent_of_code_2015_day_x;
+extern crate advent_of_code_20xx_day_x;
 
-use advent_of_code_2015_day_x::*;
+use advent_of_code_20xx_day_x::*;
 
 #[test]
 fn part_1_example() {
